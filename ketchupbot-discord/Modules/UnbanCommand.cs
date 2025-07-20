7cd925099bed529b4adcbe8cba368f716a13ba6f
smallketchup82 <ketchup@smallketchup.ca>
2025-07-20 12:53:56 -0400

    oops

--- ketchupbot-discord/Modules/UnbanCommand.cs
+++ ketchupbot-discord/Modules/UnbanCommand.cs
@@ -2,10 +2,10 @@ using Discord.Interactions;
 
 namespace ketchupbot_discord.Modules;
 
-public class PingCommand : InteractionModuleBase<SocketInteractionContext>
+public class UnbanCommand : InteractionModuleBase<SocketInteractionContext>
 {
     [SlashCommand("unban", "For the \"please unban me from galaxy\" crowd")]
-    public async Task PingAsync()
+    public async Task UnbanAsync()
     {
         await RespondAsync("go to support server we dont have any control over anything but the galaxypedia\nhttps://support.galaxypedia.org");
     }
